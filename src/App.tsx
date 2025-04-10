596663d6a3ff92a40c40ae997ddd7adb08f47eea
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2025-04-10 15:38:41 -0500

    feat: content state on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
+import { useState } from 'react';
 import './App.css';
 import Button from './components/Button.tsx';
 import Content from './components/Content.tsx';
 
 function App() {
+	const [text, setText] = useState<string>('');
+	const [title, setTitle] = useState<string>('');
+
+	function handleTitle(e: React.ChangeEvent<HTMLInputElement>) {
+		setTitle(e.target.value || '');
+	}
+	function handleContentChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+		setText(e.target.value || '');
+	}
 	return (
 		<>
 			<section className="container">
-				<aside>Carpetas</aside>
+				<aside>
+					<h1>Carpetas</h1>
+					<section>{title}</section>
+				</aside>
 				<main>
 					<section className="header">
 						<Button styleName="Save" title="Save" />
 					</section>
 					<section className="note-section">
-						<Content />
+						<Content
+							text={text}
+							onTextArea={handleContentChange}
+							onInput={handleTitle}
+						/>
 					</section>
 				</main>
 			</section>
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,27 +1,24 @@
-import { useState } from 'react';
+import { ChangeEvent } from 'react';
 import './Content.css';
-export default function Content() {
-	const [text, setText] = useState<string>('');
-	const [title, setTitle] = useState<string>('');
-
-	function handleTitle(e: React.ChangeEvent<HTMLInputElement>) {
-		setTitle(e.target.textContent || '');
-	}
-	function handleContentChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-		setText(e.target.textContent || '');
-	}
+interface ContentType {
+	text: string;
+	onTextArea: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+	onInput: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+export default function Content({ text, onTextArea, onInput }: ContentType) {
 	return (
 		<section className="content-container">
 			<input
 				type="text"
 				className="title"
 				placeholder="Título"
-				onChange={handleTitle}
+				onChange={onInput}
 			/>
 			<textarea
-				onChange={handleContentChange}
+				onChange={onTextArea}
 				className="content"
 				placeholder="Escribe algo..."
+				value={text}
 			/>
 		</section>
 	);
